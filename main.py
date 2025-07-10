9484d7bd58f37fe0c9d5715c27958cdf612e24fb
nyiri7 <nyiri.levente14@gmail.com>
2025-07-10 17:47:36 +0200

    Comment out the read_item endpoint in main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
+"""@app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    return {"item_id": item_id, "q": q}"""
